f0136fa5e1e26d1e209656c9a3cfb847e9876b05
Jessé Martins <jessemartins.ti@gmail.com>
2023-03-12 14:26:37 -0300

    Finalização do footer do projeto realtor

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import Footer from './components/footer/Footer';
 import Navigation from './components/navigation/Navigation';
 import Router from './router/Router';
 
@@ -10,6 +11,7 @@ export default function App() {
         <BrowserRouter>
             <Router/>
         </BrowserRouter>
+        <Footer/>
     </React.Fragment>
   )
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import './Footer.scss';
+import Brand from '../../assets/icons/brand-default.svg';
+import { GrFacebook } from 'react-icons/gr';
+import { BsInstagram, BsTwitter } from 'react-icons/bs';
+import { FiChevronRight } from 'react-icons/fi';
+
+export default function Footer() {
+    return (
+        <React.Fragment>
+            <footer className="re-footer">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-3 col-md-6 mb-lg-0 mb-4">
+                            <img src={Brand} alt="Brand" />
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                            <div className="re-footer-social">
+                                <a href=""><GrFacebook /></a>
+                                <a href=""><BsInstagram /></a>
+                                <a href=""><BsTwitter /></a>
+                            </div>
+                        </div>
+                        <div className="col-lg-2 col-md-6 re-footer-social mb-lg-0 mb-4">
+                            <h5>MENU</h5>
+                            <ul className="list-unstyled">
+                                <li><a href="/">Home</a></li>
+                                <li><a href="/properties">Properties</a></li>
+                                <li><a href="/about">About</a></li>
+                                <li><a href="/blog">Blog</a></li>
+                                <li><a href="/agents">Agents</a></li>
+                                <li><a href="/contact">Contact</a></li>
+                            </ul>
+                        </div>
+                        <div className="col-lg-2 col-md-6 re-footer-social mb-lg-0 mb-4">
+                            <h5>TAGS</h5>
+                            <span class="badge p-2 m-1">House</span>
+                            <span class="badge p-2 m-1">Financing</span>
+                            <span class="badge p-2 m-1">Purchase</span>
+                            <span class="badge p-2 m-1">New</span>
+                        </div>
+                        <div className="col-lg-5 col-md-6 re-footer-subscribe">
+                            <h5>SUBSCRIBE</h5>
+                            <div class="input-group input-group-md mb-3">
+                                <input type="text" class="form-control" placeholder="Your email"/>
+                                <span class="input-group-text">
+                                    <FiChevronRight/>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </React.Fragment>
+    )
+}
